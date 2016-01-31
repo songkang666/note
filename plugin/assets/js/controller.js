c13f3c30b0fadbc7b62df50942b3a409fad8436e
kevin <sokevin@cisco.com>
2016-01-31 12:40:52 +0800

    create a category

--- plugin/assets/js/controller.js
+++ plugin/assets/js/controller.js
@@ -1,7 +1,30 @@
 angular.module("noteApp")
     .controller("indexController", ["$scope", "localService", function($scope, Service) {
-        Service.queryAllNotes().then(function(res) {
-            $scope.notes = res.data;
-        }, function(res) {
-        });
+        (function init() {
+            Service.queryAllCategories().then(function(res) {
+                $scope.allCategories = res.data;
+            }, function(res) {
+                // todo
+                console.log("load failure.");
+            });
+        })();
+
+        $scope.raw = {};
+        $scope.create = function(event) {
+            var keyCode = event.keyCode;
+            var title = $scope.raw.title;
+            if(13 === keyCode) {
+                if("string" === typeof title && title.length > 0) {
+                    Service.addCategory(title).then(function(res) {
+                        $scope.raw.title = '';
+                        $scope.allCategories.entries.push(res.data);
+                        $scope.allCategories.count += 1;
+                    }, function(res) {
+                        // todo
+                        console.log("create category error.");
+                    });
+                }
+                return;
+            }
+        }
     }]);
